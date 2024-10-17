5e605daee9e01447e11e7b5a80de3b6a48f2d051
franlopezm <frans.lopez.m@gmail.com>
2024-10-17 15:31:46 +0200

    feat: Writing error messages to standard error

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let arguments = Arguments::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(arguments) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
